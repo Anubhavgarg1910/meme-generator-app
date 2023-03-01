f7ed3614fc0e371ec7651cadce208641b670be47
Anubhavgarg1910 <utkarshgarg422@example.com>
2023-03-01 17:19:30 +0530

    first-commit

---
+++ src/components/Header.js
@@ -0,0 +1,20 @@
+import React from "react"
+
+
+
+
+
+const Header = () => {
+    return (
+        <header className="header">
+            <img className="header--image" src = "https://wallpaperaccess.com/full/3567441.jpg" />
+            <h2 className="header--title">Meme Generator</h2>
+            <h4 className="header--project">React Project</h4>
+        </header>
+    )
+
+}
+
+export default Header
+
+
